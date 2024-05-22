aa0a146590fd02c1c6d9fb23cfb62aa83c9071c5
nikosoo <130840899+nikosoo@users.noreply.github.com>
2024-05-22 14:24:10 +0300

    asd

--- frontendshopproj/src/footer/Footer.jsx
+++ frontendshopproj/src/footer/Footer.jsx
@@ -118,7 +118,7 @@ function Footer() {
           </a>
         </div>
         <p class="mt-8 text-base leading-6 text-center text-gray-400">
-          © 2021 TasteHaven, Inc. All rights reserved.
+          © 2024 TasteHaven, Inc. All rights reserved.
         </p>
       </div>
     </section>
