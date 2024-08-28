471733843241be85cdb370fed8f2905579c4cf89
nikosoo <130840899+nikosoo@users.noreply.github.com>
2024-08-28 14:53:28 +0300

    asds

--- backendshopproj/server.js
+++ backendshopproj/server.js
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
@@ -10,8 +7,14 @@ import collectionRoutes from "./routes/collectionRoutes.js";
 const app = express();
 
 // Middleware
+app.use(
+  cors({
+    origin: "https://food-proj-hwg6.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
-app.use(cors());
 
 // Connect to the database
 connectDB();
